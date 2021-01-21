a9672d7ae83d031efac93d0f17ee9870ddf56c96
Krzysztof <lema10@o2.pl>
2021-01-21 18:09:25 +0100

    separate view from view controller

---
+++ Experiments/Experiments/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  Experiments
+//
+//  Created by Krzysztof Lema on 21/01/2021.
+//
+
+import UIKit
+
+class FirstView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
