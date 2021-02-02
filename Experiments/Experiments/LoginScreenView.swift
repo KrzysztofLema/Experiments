7d8e2c829c406f3c3c0f4c1d007fe9d5b5ca2149
Krzysztof <Krzysztof.Lema@fieldcode.com>
2021-02-02 20:12:48 +0100

    preparing login screen

--- Experiments/Experiments/LoginScreenView.swift
+++ Experiments/Experiments/LoginScreenView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct LoginScreenView: View {
     
+    @State var login: String = ""
+    @State var password: String = ""
+    
     let blueColor = Color(.systemBlue)
     var body: some View {
         VStack {
-            blueColor
+            TextField("Login", text: $login)
+                .padding()
+            TextField("Password", text: $password)
+                .padding()
+            Button("Log in") {}
+                .buttonStyle(FilledButton())
         }
     }
 }
 
+
 struct LoginScreenView_Previews: PreviewProvider {
     static var previews: some View {
         LoginScreenView()
+    }
+}
+
+struct FilledButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
     }
 }
