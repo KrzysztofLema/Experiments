794ee9f07fb27b79e6c9d57e5bca75233c2a64ed
Krzysztof <Krzysztof.Lema@fieldcode.com>
2021-02-11 00:48:23 +0100

    Screens Connected

---
+++ Experiments/Experiments/Screens/MainScreen/MainViewController.swift
@@ -0,0 +1,99 @@
+//
+//  MainViewController.swift
+//  Experiments
+//
+//  Created by Krzysztof Lema on 10/02/2021.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+enum ViewToPresent {
+    case splashScreen
+    case loginScreen
+}
+
+class MainViewController: UIViewController {
+    
+    let viewModel: MainViewModel
+    let loginViewController = LoginScreenViewController()
+    let splashScreenViewController = SplashViewController()
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    init(viewModel: MainViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func present(_ view: ViewToPresent) {
+        switch view {
+        case .loginScreen:
+            presentLoginScreen()
+        case .splashScreen:
+            presentSplashScreen()
+        }
+    }
+    
+    override func viewDidLoad() {
+        bind()
+    }
+    
+    func subscribe(to publisher: AnyPublisher<ViewToPresent, Never>) {
+        publisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] view in
+                guard let self = self else { return }
+                self.present(view)
+            }.store(in: &subscriptions)
+    }
+    
+    private func bind() {
+        let publisher = viewModel.$view.removeDuplicates().eraseToAnyPublisher()
+        subscribe(to: publisher)
+    }
+}
+
+private extension MainViewController {
+    func presentLoginScreen() {
+        loginViewController.modalPresentationStyle = .fullScreen
+        present(loginViewController, animated: true) { [weak self] in
+            self?.remove(self?.splashScreenViewController)
+        }
+    }
+    
+    func presentSplashScreen() {
+        addFullScreen(childViewController: splashScreenViewController)
+    }
+}
+
+private extension UIViewController {
+    func addFullScreen(childViewController child: UIViewController) {
+        guard child.parent == nil else {
+            return
+        }
+        addChild(child)
+        view.addSubview(child.view)
+        child.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            child.view.topAnchor.constraint(equalTo: view.topAnchor),
+            child.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            child.view.rightAnchor.constraint(equalTo: view.rightAnchor),
+            child.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        child.didMove(toParent: self)
+    }
+    
+    func remove(_ child: UIViewController?) {
+        guard let child = child, child.parent != nil else { return }
+
+        child.willMove(toParent: nil)
+        child.view.removeFromSuperview()
+        child.removeFromParent()
+    }
+}
